a930ec36740cdec214f774c768eb2c7c1e009bd0
jean-philippe Heurteux <158490322+giblimdev@users.noreply.github.com>
2025-05-30 03:17:09 +0200

    page2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BgLanding from "@/components/layout/BgLanding";
 import Header from "@/components/header/Header";
 
 const geistSans = Geist({
